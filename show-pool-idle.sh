d8c9090dee196c986cc50305928c660c8c3aaf1d
M Fauzilkamil Zainuddin <m.fauzilkamil@gmail.com>
2024-01-03 00:03:00 +0800

    tambah script untuk show pgpool idle dan wait

---
+++ show-pool-idle.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# https://github.com/ApOgEE
+# Command untuk display pgpool idle
+# cara guna:
+# watch -n 5 ./show-pool-idle.sh
+# pastikan password dah chmod 600 dalam ~/.pgpass
+# contoh pgpass:
+# *.*:postgres:pgpoolusr:passwordsaya
+PGUSER=pgpoolusr
+PGHOST=ha
+
+psql -h $PGHOST -U $PGUSER postgres -c "SHOW pool_processes;" | grep -v "rows\|row\|^$" | grep Idle | wc -l
---
+++ show-pool-wait.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# https://github.com/ApOgEE
+# Command untuk display pgpool wait
+# cara guna:
+# watch -n 5 ./show-pool-wait.sh
+# pastikan password dah chmod 600 dalam ~/.pgpass
+# contoh pgpass:
+# *.*:postgres:pgpoolusr:passwordsaya
+PGUSER=pgpoolusr
+PGHOST=ha
+
+psql -h $PGHOST -U $PGUSER postgres -c "SHOW pool_processes;" | grep -v "rows\|row\|^$" | grep Wait | wc -l
