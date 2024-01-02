49bded2b6e62d25791cdb5d49b01c346cf3a0ddb
M Fauzilkamil Zainuddin <m.fauzilkamil@gmail.com>
2024-01-03 00:16:51 +0800

    tambah script untuk tengok pgpool nodes

---
+++ show-pool-nodes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# https://github.com/ApOgEE
+# Command untuk display pgpool nodes
+# cara guna:
+# watch -n 1 ./show-pool-nodes.sh
+# pastikan password dah chmod 600 dalam ~/.pgpass
+# contoh pgpass:
+# *.*:postgres:pgpoolusr:passwordsaya
+PGUSER=pgpoolusr
+PGHOST=ha
+
+psql -h $PGHOST -U $PGUSER postgres -c "\x" -c "show pool_nodes;"
