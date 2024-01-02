6af9e32a427d59c5ab364b8e306201a2b88d809e
M Fauzilkamil Zainuddin <m.fauzilkamil@gmail.com>
2024-01-02 23:34:25 +0800

    update haproxy stat script

--- disp_haproxy_stat.py
+++ disp_haproxy_stat.py
@@ -7,22 +7,29 @@ import csv
 import sys
 from prettytable import PrettyTable
 
-# Function to filter the required data
-def filter_data(row):
-    return [row['# pxname'] + '/' + row['svname'], row['scur'], row['status']]
+# Function to create and return a formatted table
+def create_table(field_names):
+    table = PrettyTable()
+    table.field_names = field_names
+    table.align = "l"
+    return table
 
 # Read CSV from stdin
 reader = csv.DictReader(sys.stdin)
 
-# Create a PrettyTable instance
-table = PrettyTable()
-table.field_names = ["Name", "Current Sessions", "Status"]
-table.align = "l"
+# Create tables for frontend and backend
+frontend_table = create_table(["Frontend Name", "Current Sessions","Max Sessions", "Status"])
+backend_table = create_table(["Backend Name/Node", "Current Sessions","Max Sessions", "Status"])
 
-# Filter and add rows to the table for frontend and backend
+# Filter and add rows to the respective tables
 for row in reader:
-    if row['svname'] in ['FRONTEND', 'BACKEND']:
-        table.add_row(filter_data(row))
+    if row['svname'] == 'FRONTEND' and row['# pxname'] != 'stats':
+        frontend_table.add_row([row['# pxname'], row['scur'], row['smax'], row['status']])
+    elif (row['svname'] == 'BACKEND' or row['svname'] != 'FRONTEND') and row['# pxname'] != 'stats':
+        backend_table.add_row([row['# pxname'] + '/' + row['svname'], row['scur'], row['smax'],row['status']])
 
-# Print the table
-print(table)
+# Print the tables
+print("Frontend Statistics:")
+print(frontend_table)
+print("\nBackend Statistics:")
+print(backend_table)
